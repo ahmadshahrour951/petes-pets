813bf7aac7c3365ab0d548f0a51e8ba7e15f3999
shahrour <ahmadshahrour951@gmail.com>
2021-04-20 17:45:12 -0700

    Implemented S3 file uploads

--- routes/pets.js
+++ routes/pets.js
@@ -1,3 +1,34 @@
+// UPLOADING TO AWS S3
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const Upload = require('s3-uploader');
+
+const client = new Upload(process.env.S3_BUCKET, {
+  aws: {
+    path: 'pets/avatar',
+    region: process.env.S3_REGION,
+    acl: 'public-read',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+  cleanup: {
+    versions: true,
+    original: true,
+  },
+  versions: [
+    {
+      maxWidth: 400,
+      aspect: '16:10',
+      suffix: '-standard',
+    },
+    {
+      maxWidth: 300,
+      aspect: '1:1',
+      suffix: '-square',
+    },
+  ],
+});
+
 // MODELS
 const Pet = require('../models/pet');
 
@@ -13,18 +44,31 @@ module.exports = (app) => {
   });
 
   // CREATE PET
-  app.post('/pets', (req, res) => {
+  app.post('/pets', upload.single('avatar'), (req, res, next) => {
     var pet = new Pet(req.body);
+    pet.save(function (err) {
+      if (req.file) {
+        // Upload the images
+        client.upload(req.file.path, {}, function (err, versions, meta) {
+          if (err) {
+            return res.status(400).send({ err: err });
+          }
 
-    pet
-      .save()
-      .then((pet) => {
+          // Pop off the -square and -standard and just use the one URL to grab the image
+          versions.forEach(function (image) {
+            var urlArray = image.url.split('-');
+            urlArray.pop();
+            var url = urlArray.join('-');
+            pet.avatarUrl = url;
+            pet.save();
+          });
+
+          res.send({ pet: pet });
+        });
+      } else {
         res.send({ pet: pet });
-      })
-      .catch((err) => {
-        // STATUS OF 400 FOR VALIDATIONS
-        res.status(400).send(err.errors);
-      });
+      }
+    });
   });
 
   // SHOW PET
@@ -79,45 +123,45 @@ module.exports = (app) => {
     });
   });
 
-    app.post('/pets/:id/purchase', (req, res) => {
-      console.log(req.body);
-      // Set your secret key: remember to change this to your live secret key in production
-      // See your keys here: https://dashboard.stripe.com/account/apikeys
-      var stripe = require('stripe')(process.env.PRIVATE_STRIPE_API_KEY);
+  app.post('/pets/:id/purchase', (req, res) => {
+    console.log(req.body);
+    // Set your secret key: remember to change this to your live secret key in production
+    // See your keys here: https://dashboard.stripe.com/account/apikeys
+    var stripe = require('stripe')(process.env.PRIVATE_STRIPE_API_KEY);
 
-      // Token is created using Checkout or Elements!
-      // Get the payment token ID submitted by the form:
-      const token = req.body.stripeToken; // Using Express
+    // Token is created using Checkout or Elements!
+    // Get the payment token ID submitted by the form:
+    const token = req.body.stripeToken; // Using Express
 
-      // req.body.petId can become null through seeding,
-      // this way we'll insure we use a non-null value
-      let petId = req.body.petId || req.params.id;
+    // req.body.petId can become null through seeding,
+    // this way we'll insure we use a non-null value
+    let petId = req.body.petId || req.params.id;
 
-      Pet.findById(petId).exec((err, pet) => {
-        if (err) {
+    Pet.findById(petId).exec((err, pet) => {
+      if (err) {
+        console.log('Error: ' + err);
+        res.redirect(`/pets/${req.params.id}`);
+      }
+      const charge = stripe.charges
+        .create({
+          amount: pet.price * 100,
+          currency: 'usd',
+          description: `Purchased ${pet.name}, ${pet.species}`,
+          source: token,
+        })
+        .then((chg) => {
+          // Convert the amount back to dollars for ease in displaying in the template
+          const user = {
+            email: req.body.stripeEmail,
+            amount: chg.amount / 100,
+            petName: pet.name,
+          };
+          // Call our mail handler to manage sending emails
+          mailer.sendMail(user, req, res);
+        })
+        .catch((err) => {
           console.log('Error: ' + err);
-          res.redirect(`/pets/${req.params.id}`);
-        }
-        const charge = stripe.charges
-          .create({
-            amount: pet.price * 100,
-            currency: 'usd',
-            description: `Purchased ${pet.name}, ${pet.species}`,
-            source: token,
-          })
-          .then((chg) => {
-            // Convert the amount back to dollars for ease in displaying in the template
-            const user = {
-              email: req.body.stripeEmail,
-              amount: chg.amount / 100,
-              petName: pet.name,
-            };
-            // Call our mail handler to manage sending emails
-            mailer.sendMail(user, req, res);
-          })
-          .catch((err) => {
-            console.log('Error: ' + err);
-          });
-      });
+        });
     });
-}
+  });
+};
